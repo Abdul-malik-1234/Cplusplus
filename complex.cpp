f0cdda0b8c116e3946d7621a93a81e54fc8d063e
Abdul Malik Pasha <82267578+Abdul-malik-1234@users.noreply.github.com>
2023-04-08 19:46:11 +0530

    operator overloading

--- complex.cpp
+++ complex.cpp
@@ -1,6 +1,25 @@
 #include<iostream>
 using namespace std;
+class Complex{
+    private:
+    int real;
+    int img;
+    public:
+    Complex(int r=0,int i=0){
+        real=r;
+        img=i;
+    }
+    void display(){
+        cout<<real<<"+i"<<img<<endl;
+    }
+    Complex operator+(Complex x){
+        Complex temp(real+x.real,img+x.img);
+        return temp;
+    }
+};
 int main(){
-
+    Complex c1(1,2),c2(2,3);
+    Complex c3=c1+c2;
+    c3.display();
     return 0;
 }
