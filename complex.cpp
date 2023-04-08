852ea8bc0c39ec1554378e3c5c40954872ec5a4a
Abdul Malik Pasha <82267578+Abdul-malik-1234@users.noreply.github.com>
2023-04-08 20:07:17 +0530

    added operator overloading

--- complex.cpp
+++ complex.cpp
@@ -16,10 +16,32 @@ class Complex{
         Complex temp(real+x.real,img+x.img);
         return temp;
     }
+    Complex operator*(Complex x){
+        Complex temp(real*x.real,img*x.img);
+        return temp;
+    }
+    Complex operator-(Complex x){
+        Complex temp(real-x.real,img-x.img);
+        return temp;
+    }
+    Complex operator/(Complex x){
+        Complex temp(real/x.real,img/x.img);
+        return temp;
+    }
 };
 int main(){
     Complex c1(1,2),c2(2,3);
     Complex c3=c1+c2;
+    cout<<"Addition of c1 and c2 gives ";
+    c3.display();
+    c3=c1-c2;
+    cout<<"Subtraction of c1 and c2 gives ";
+    c3.display();
+    c3=c1*c2;
+    cout<<"Multiplication of c1 and c2 gives ";
+    c3.display();
+    c3=c1/c2;
+    cout<<"Division of c1 and c2 gives ";
     c3.display();
     return 0;
 }
