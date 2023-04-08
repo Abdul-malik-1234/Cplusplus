e26bb40c081361dbbbdd18e22126d9c53ea7b22c
Abdul Malik Pasha <82267578+Abdul-malik-1234@users.noreply.github.com>
2023-04-08 21:00:13 +0530

    friend function

--- friendcomplex.cpp
+++ friendcomplex.cpp
@@ -13,13 +13,20 @@ class Complex{
         cout<<real<<" +i"<<imag;
     }
     friend Complex operator+(Complex c1,Complex c2);
+    friend ostream& operator<<(ostream& o,Complex& c);
+    
 };
+ostream& operator<<(ostream& o,Complex& c){
+        o<<c.real<<"+i"<<c.imag;
+        return o;
+    }
 Complex operator+(Complex c1,Complex c2){
     Complex temp(c1.real+c2.real,c1.imag+c2.imag);
     return temp;
 }
 int main(){
     Complex c1(1,2),c2(3,4);
-    (c1+c2).display();
+    Complex c3=c1+c2;
+    cout<<c3;
     return 0;
 }
