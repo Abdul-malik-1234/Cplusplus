1d8847f13c3adcd4f1ff9a6320d38bd74d55def9
Abdul Malik Pasha <82267578+Abdul-malik-1234@users.noreply.github.com>
2023-04-08 20:22:21 +0530

    friend function

---
+++ friendcomplex.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+class Complex{
+    private:
+    int real;
+    int imag;
+    public:
+    Complex(int r,int i){
+        real=r;
+        imag=i;
+    }
+    void display(){
+        cout<<real<<" +i"<<imag;
+    }
+    friend Complex operator+(Complex c1,Complex c2);
+};
+Complex operator+(Complex c1,Complex c2){
+    Complex temp(c1.real+c2.real,c1.imag+c2.imag);
+    return temp;
+}
+int main(){
+    Complex c1(1,2),c2(3,4);
+    (c1+c2).display();
+    return 0;
+}
